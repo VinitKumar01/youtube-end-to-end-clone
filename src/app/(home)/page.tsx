45961d6cc1a4228fe9890df52dd3ca3832a86fb4
vinit <vinitk81144@gmail.com>
2025-04-13 23:30:24 +0530

    added trpc setup & configurations, ratelimiter etc.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import { HydrateClient, trpc } from "@/trpc/server";
+import PageClient from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-export default function Home() {
-  return <div>Videos will be loaded here!</div>;
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Vinit" });
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
